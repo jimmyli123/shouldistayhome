efa0123ce994b54beb59149cfb7688550c0d5229
Jimmy Li <lijjimmy@gmail.com>
2023-01-02 14:29:24 -0500

    progress bar changes based on how much capacity there is.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 document.querySelector("#enter").addEventListener('click', addOne)
 document.querySelector("#leave").addEventListener('click', removeOne)
 let count = 0;
-let maxCapacity = 1000;
-let oneThird = 333;
-let twoThird = 666;
+let maxCapacity = 10;
+let oneFourth = maxCapacity/4;
+let twoFourth = oneFourth * 2;
+let threeFourth = oneFourth * 3;
 
 
 function addOne() {
@@ -13,16 +14,8 @@ function addOne() {
     else {
         count +=1;
         document.querySelector("#showCount").innerHTML = count;
-        let progressBar = document.querySelector(".progress-bar")
-        if (count < oneThird) {
-            progressBar.setAttribute("style","width: 33%")
-        }
-        else if (count < twoThird) {
-            progressBar.setAttribute("style","width: 66%")
-        }
-        else {
-            progressBar.setAttribute("style","width: 90%")
-        }
+        let progressBar = document.querySelector("#progress_bar")
+        checkCounter(count)
     }
 
 }
@@ -34,6 +27,31 @@ function removeOne() {
     else {
         count -=1;
         document.querySelector("#showCount").innerHTML = count;
+        checkCounter(count)
     }
     
+}
+
+function checkCounter(counter) {
+    let progressBar = document.querySelector("#progress_bar")
+    if (counter ===0) {
+        progressBar.setAttribute("style","width: 0%")
+        progressBar.className=""
+    }
+    else if (counter <= oneFourth) {
+        progressBar.setAttribute("style","width: 25%")
+        progressBar.className="progress-bar bg-success"
+    }
+    else if (counter <= twoFourth) {
+        progressBar.setAttribute("style","width: 50%")
+        progressBar.className="progress-bar bg-info"
+    }
+    else if (counter <=threeFourth) {
+        progressBar.setAttribute("style","width: 75%")
+        progressBar.className="progress-bar bg-warning"
+    }
+    else {
+        progressBar.setAttribute("style","width: 100%")
+        progressBar.className="progress-bar bg-danger"
+    }
 }
